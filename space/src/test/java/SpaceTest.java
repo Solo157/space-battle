c1125941a5a63816b066c324e703171fcf34a77d
kirill <kirill.vasyukov@eltex.loc>
2025-10-13 18:32:26 +0700

    init

---
+++ space/src/test/java/SpaceTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class SpaceTest {
+
+    /**
+     * Проверяет, что для уравнения x^2 + 1 = 0 корней нет (пустой массив).
+     */
+    @Test
+    public void testNoRootsForEquation_x2Plus1_equalsZero() {
+
+    }
+
+}
