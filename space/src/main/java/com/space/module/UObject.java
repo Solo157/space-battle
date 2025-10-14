ceec22e69ab6dac11d7b4652e2d549df1cb0ed07
kirill <kirill.vasyukov@eltex.loc>
2025-10-14 19:51:41 +0700

    init 2

---
+++ space/src/main/java/com/space/module/UObject.java
@@ -0,0 +1,9 @@
+package com.space.module;
+
+public interface UObject {
+
+    Object getProperty(String property);
+
+    void setProperty(String key, Object newValue);
+
+}
---
+++ space/src/main/java/com/space/module/Vector.java
@@ -0,0 +1,39 @@
+package com.space.module;
+
+public class Vector {
+
+    private int dx;
+    private int dy;
+
+    public Vector(int dx, int dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    public int getX() {
+        return this.dx;
+    }
+
+    public int getY() {
+        return this.dy;
+    }
+
+    public void setX(int dx) {
+        this.dx = dx;
+    }
+
+    public void setY(int dy) {
+        this.dy = dy;
+    }
+
+    public static Point plus(Point location, Vector velocity) {
+        if (location == null || velocity == null) {
+            throw new RuntimeException();
+        }
+
+        location.setX(location.getX() + velocity.dx);
+        location.setY(location.getY() + velocity.dy);
+        return location;
+    }
+
+}
